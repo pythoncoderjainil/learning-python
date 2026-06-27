e0a075c6f574ac0568a933687cde3e65a9e791ca
pythoncoderjainil <164878864+pythoncoderjainil@users.noreply.github.com>
2026-06-27 21:31:03 +0530

    user input addition program

--- program on user input.py
+++ program on user input.py
@@ -1,4 +1,4 @@
 
-Number_1=input("Enter the first number: ")
-Number_2=input("Enter the second number: ")
+Number_1=input("Enter the first number: ")# FOR 1ST NO. INPUT
+Number_2=input("Enter the second number: ")# FOR 2ND NO. INPUT
 print("The sum of the two numbers is: ",int(Number_1) + int(Number_2))
