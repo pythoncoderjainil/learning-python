d217c00a468a317328ca1d3634fcd666bdc37e92
pythoncoderjainil <164878864+pythoncoderjainil@users.noreply.github.com>
2026-06-27 21:45:38 +0530

    implicit explicit conversions

---
+++ implicit explicit conversion.py
@@ -0,0 +1,8 @@
+# implicit conversion
+print(4+5.5)
+print(6+7+3j)
+
+# explicit conversion
+Number_1=input("Enter the first number: ")
+Number_2=input("Enter the second number: ")
+int(Number_1) + int(Number_2)
