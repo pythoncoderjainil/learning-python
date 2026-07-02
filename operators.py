8ca9755bd0a93e78b0c497e611fe4ad7c5915424
pythoncoderjainil <164878864+pythoncoderjainil@users.noreply.github.com>
2026-07-02 14:22:52 +0530

    All operators in python

---
+++ operators.py
@@ -0,0 +1,42 @@
+x= 7
+y= 4
+
+#Arithmetic operators
+print(x + y) #addition
+print(x - y) #subtraction
+print(x*y)# multiplication
+print(x/y) #division
+print(x//y) #floor division
+print(x%y) #modulus
+print(x**y) #exponentiation
+
+#Comparison operators
+print(x > y) #greater than
+print(x < y) #less than
+print(x >= y) #greater than or equal to
+print(x <= y) #less than or equal to
+print(x == y) #equal to
+print(x != y) #not equal to
+
+#Bitwise operators
+print(x & y) #bitwise AND
+print(x | y) #bitwise OR
+print(x ^ y) #bitwise XOR
+print(~x) #bitwise NOT
+print(y << 4) #left shift
+print(x >> 7) #right shift
+
+
+#Assignment operators
+a= 3
+a&=3
+a+=3
+a*=3
+a/=3
+#all the arithemetic operators can be used with assignment operators
+
+#Membership operators
+a= [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+print(3 in a) #in
+print(a in b)
